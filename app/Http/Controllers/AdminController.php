0e905484f42898cc1d831b53a73aa0c3a63e0d47
rupp <rupp150406>
2025-11-24 10:45:23 +0700

    last video 7 cuma belum kelar, karena nambah function add brands admin yang belum work gtw knapa nanti di fix yh ganteng

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -4,6 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Models\Brand;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Image\Laravel\Facades\Image;
 
 class AdminController extends Controller
 {
@@ -17,5 +21,40 @@ class AdminController extends Controller
     {
         $brands = Brand::orderBy('id', 'DESC')->paginate(10);
         return view('admin.brands', compact('brands'));
+    }
+
+    public function brand_add()
+    {
+        return view('admin.brand-add');
+    }
+
+    public function brand_store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'slug' => 'required|string|max:255|unique:brands,slug',
+            'image' => 'mimes:jpeg,png,jpg,gif|max:2048',
+        ]);
+
+        $brand = new Brand();
+        $brand->name = $request->name;
+        $brand->slug = Str::slug($request->name);
+        $image = $request->file('image');
+        $file_extention = $request->file('image')->extension();
+        $file_name = Carbon::now()->timestamp . '.' . $file_extention;
+        $this->GenerateBrandThumbnailsImage($image, $file_name);
+        $brand->image = $file_name;
+        $brand->save();
+        return redirect()->route('admin.brands')->with('status', 'Brand has been added successfully.');
+    }
+
+    public function GenerateBrandThumbnailsImage($image, $imageName)
+    {
+        $destinationPath = public_path('uploads/brands');
+        $img = Image::read($image->path());
+        $img = $img->cover(124, 124, "top");
+        $img = $img->resize(124, 124, function ($constraint) {
+            $constraint->aspectRatio();
+        })->save($destinationPath . '/' . $imageName);
     }
 }
