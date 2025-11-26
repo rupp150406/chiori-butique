6d7e4feeff822a5bfca8053e78374d2dcbb822cd
rupp <rupp150406>
2025-11-26 09:26:51 +0700

    video 9 : 3.56

--- resources/views/admin/brands.blade.php
+++ resources/views/admin/brands.blade.php
@@ -67,7 +67,7 @@
                                                         <td><a href="#" target="_blank">1</a></td>
                                                         <td>
                                                             <div class="list-icon-function">
-                                                                <a href="#">
+                                                                <a href="{{ route('admin.brand.edit',['id' => $brand->id]) }}">
                                                                     <div class="item edit">
                                                                         <i class="icon-edit-3"></i>
                                                                     </div>
